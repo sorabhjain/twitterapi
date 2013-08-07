602945db1db10e04af2848088023476d4fd1fe3b
sorabh jain <sorabh.52@gmail.com>
2013-08-07 19:48:39 +0530

    library for making o-auth call with twitter

--- TwitterAPI.inc.php
+++ TwitterAPI.inc.php
@@ -1,8 +1,6 @@
 <?php
 
 class TwitterAPI {
-    private $consumerKey = 'd2Qnfm17U2FwOGXSt55Vw'; //consumer key goes here
-    private $consumerSecret = 'fh7FMMjXDeDUaKcDzRTasONCQqVa84N0ge22NVYw'; //consumer secret goes here  
     private $consumerKey = ' '; //consumer key goes here
     private $consumerSecret = ' '; //consumer secret goes here  
     private $nonceSecret = 'erhhththyt'; //any random string
